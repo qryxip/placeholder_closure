b492ee78aee4f122a2917880d3daf72f6abadfcf
Ryo Yamashita <qryxip@gmail.com>
2021-06-19 16:32:02 +0900

    Also provide as `λ!`

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,9 @@ mod lambda;
 pub fn lambda(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     lambda::lambda(input.into()).into()
 }
+
+#[rustversion::since(1.53)]
+#[proc_macro]
+pub fn λ(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    lambda::lambda(input.into()).into()
+}
