1b415bce03e1800a8ea3a38aa3415186e35b9466
Ryo Yamashita <qryxip@gmail.com>
2021-06-19 16:48:21 +0900

    Enable constructing closures with `move`

--- src/lambda.rs
+++ src/lambda.rs
@@ -1,11 +1,22 @@
-use proc_macro2::{TokenStream, TokenTree};
+use proc_macro2::{Delimiter, TokenStream, TokenTree};
 use quote::{quote, ToTokens as _};
 use std::iter;
 
 pub(crate) fn lambda(input: TokenStream) -> TokenStream {
+    let (movability, input) = movability(input);
     let mut args = vec![];
     let body = replace_dollars(input, &mut args);
-    quote!(|#(#args),*| #body)
+    quote!(#movability |#(#args),*| #body)
+}
+
+fn movability(input: TokenStream) -> (TokenStream, TokenStream) {
+    let input = input.into_iter().collect::<Vec<_>>();
+    if let [TokenTree::Ident(ident), TokenTree::Group(group)] = &*input {
+        if ident == "move" && group.delimiter() == Delimiter::Brace {
+            return (quote!(#ident), group.stream());
+        }
+    }
+    (quote!(), input.into_iter().collect())
 }
 
 fn replace_dollars(input: TokenStream, args: &mut Vec<syn::Ident>) -> TokenStream {
