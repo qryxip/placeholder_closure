4daff8572bc2b7e02f521bb534ce357ab9d4654a
Ryo Yamashita <qryxip@gmail.com>
2021-06-19 16:31:34 +0900

    Fix for `Group`

--- src/lambda.rs
+++ src/lambda.rs
@@ -1,35 +1,27 @@
-use proc_macro2::{Spacing, TokenStream, TokenTree};
+use proc_macro2::{TokenStream, TokenTree};
 use quote::{quote, ToTokens as _};
-use std::{iter, mem};
+use std::iter;
 
 pub(crate) fn lambda(input: TokenStream) -> TokenStream {
-    let (mut args, mut body) = (vec![], TokenStream::new());
-    replace_dollars(input, &mut args, &mut body, &mut 0);
+    let mut args = vec![];
+    let body = replace_dollars(input, &mut args);
     quote!(|#(#args),*| #body)
 }
 
-fn replace_dollars(
-    input: TokenStream,
-    args: &mut Vec<syn::Ident>,
-    body: &mut TokenStream,
-    n: &mut u32,
-) {
+fn replace_dollars(input: TokenStream, args: &mut Vec<syn::Ident>) -> TokenStream {
+    let mut body = TokenStream::new();
     for tt in input {
         match tt {
-            TokenTree::Group(group) => replace_dollars(group.to_token_stream(), args, body, n),
-            TokenTree::Punct(punct)
-                if punct.as_char() == '$' && punct.spacing() == Spacing::Alone =>
-            {
-                let arg = syn::Ident::new(&name(n), punct.span());
+            TokenTree::Group(group) => body.extend(iter::once(TokenTree::Group(
+                proc_macro2::Group::new(group.delimiter(), replace_dollars(group.stream(), args)),
+            ))),
+            TokenTree::Punct(punct) if punct.as_char() == '$' => {
+                let arg = syn::Ident::new(&format!("__{}", args.len()), punct.span());
                 body.extend(arg.to_token_stream());
                 args.push(arg);
             }
             tt => body.extend(iter::once(tt)),
         }
     }
-
-    fn name(n: &mut u32) -> String {
-        let n_next = *n + 1;
-        format!("__{}", mem::replace(n, n_next))
-    }
+    body
 }
